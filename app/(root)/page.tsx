bf97256f707ba0617f0d5daa84ca24274f153e47
mike <unknownentity5648@gmail.com>
2025-01-13 19:11:04 +0200

    Built home screen UI

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import BookOverview from '@/components/BookOverview';
+import BookList from '@/components/BookList';
+import { sampleBooks } from '@/constants';
+
+const Home = () => {
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-20"
+      />
+    </>
+  );
+};
+export default Home;
